5683befbd07e1579aa018abb8239cbc8ef977434
akmhmgc <akimuhamaguchi@icloud.com>
2023-01-03 15:15:12 +0900

    feat: useEffectの引数を指定して副作用をスキップする

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -3,12 +3,12 @@ import "./TripList.css";
 
 export default function TripList() {
   const [trips, setTrips] = useState([]);
-  console.log(trips);
+  const [url, setUrl] = useState("http://localhost:3000/trips");
   useEffect(() => {
-    fetch("http://localhost:3000/trips")
+    fetch(url)
       .then((response) => response.json())
       .then((json) => setTrips(json));
-  });
+  }, [url]);
   return (
     <div className="trip-list">
       <h2>Trip list</h2>
@@ -20,6 +20,10 @@ export default function TripList() {
           </li>
         ))}
       </ul>
+      <div className="filters">
+        <button onClick={()=> setUrl("http://localhost:3000/trips?loc=europe")}>European Trips</button>
+        <button onClick={()=> setUrl("http://localhost:3000/trips")}>All trips</button>
+      </div>
     </div>
   );
 }
