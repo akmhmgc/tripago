3cfad3bd3cfe5ddc4bf3e5c69bc9e4f48c871daf
akmhmgc <akimuhamaguchi@icloud.com>
2023-01-03 15:31:19 +0900

    refactor: useEffectの引数に関数を指定する方法

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,14 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import "./TripList.css";
 
 export default function TripList() {
   const [trips, setTrips] = useState([]);
   const [url, setUrl] = useState("http://localhost:3000/trips");
-  useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((json) => setTrips(json));
+  const fetchTrips = useCallback(async () => {
+    const response = await fetch(url);
+    const json = await response.json();
+    setTrips(json);
   }, [url]);
+  useEffect(() => {
+    fetchTrips();
+  }, [fetchTrips]);
   return (
     <div className="trip-list">
       <h2>Trip list</h2>
@@ -21,8 +24,14 @@ export default function TripList() {
         ))}
       </ul>
       <div className="filters">
-        <button onClick={()=> setUrl("http://localhost:3000/trips?loc=europe")}>European Trips</button>
-        <button onClick={()=> setUrl("http://localhost:3000/trips")}>All trips</button>
+        <button
+          onClick={() => setUrl("http://localhost:3000/trips?loc=europe")}
+        >
+          European Trips
+        </button>
+        <button onClick={() => setUrl("http://localhost:3000/trips")}>
+          All trips
+        </button>
       </div>
     </div>
   );
